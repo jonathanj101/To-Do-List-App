d625b8fbb092f848c28b92775ae6b79399605c20
jonathanj101 <jonatjimen@gmail.com>
2020-08-26 16:20:11 -0400

    updated
    updated

--- main.js
+++ main.js
@@ -5,37 +5,74 @@ let unordrd = document.querySelector("#main-lists__container")
 let listsItem = document.querySelector("#lists-item")
 
 /* EVENT LISTENER FOR USER INPUT*/
-userInput.addEventListener('keydown', () => {
+userInput.addEventListener('keydown', (event) => {
     if (event.keyCode === 13) {
-        let list = document.createElement('li')
+        let listItem = document.createElement('li')
+        let listBtnItem = document.createElement('li')
         let spanInputValue = document.createElement('span')
-        let spanRemove = document.createElement('span')
+        let btnRemove = document.createElement('button')
 
-        list.id = 'lists-item'
-        spanInputValue.id = "inputValue"
-        spanRemove.id = 'remove'
+        listItem.id = 'lists-item'
+        btnRemove.id = 'remove'
+        listBtnItem.id = 'lists-item'
+        spanInputValue.id = 'inputValue'
+        btnRemove.className = userInput.value
+
 
         let spnUserText = document.createTextNode(userInput.value)
 
 
+
         spanInputValue.appendChild(spnUserText)
+        listBtnItem.appendChild(btnRemove)
 
 
-        list.appendChild(spanInputValue)
+        unordrd.insertAdjacentElement("afterbegin", listItem)
+        listItem.insertAdjacentElement("beforeend", btnRemove)
+        listItem.insertAdjacentElement('afterbegin', spanInputValue)
+        btnRemove.innerHTML = '&times;'
 
-        unordrd.insertAdjacentElement("afterbegin", list)
-        list.insertAdjacentElement('beforeend', spanRemove)
-        spanRemove.innerHTML = '&times;'
-
-        console.log(list, spanInputValue, spanRemove)
+        //console.log(spnUserText, spanInputValue, spanRemove)
     }
-    console.log(`keyCode = ${event.keyCode} code = ${event.code} `)
+    //console.log(`keyCode = ${event.keyCode} code = ${event.code} `)
 })
 
 /* FUNCTION TO DELETE TASK*/
 
 
+userInput.addEventListener('keyup', (event) => {
+    if (event.keyCode === 13) {
+        let listItems = document.querySelectorAll('li')
+        let btnRemove = document.getElementsByTagName('button')
+        let spanInputValue = document.createElement('span')
+        let items = []
 
+        for (let i = 0; i < listItems.length; i++) {
+            items.push(listItems[i])
+        }
+        console.log(items)
+        console.log(listItems[0])
+
+        /*
+        let item;
+        for (let j = 0; j < items.length; j++) {
+            item += items[j].innerHTML
+        }
+        console.log(item)
+        */
+        //console.log(unordrd)
+        //console.log(listItems)
+        /*
+        for (let i = 0; i < listItems.length; i++) {
+            items.push(listItems[i].innerHTML)
+        }
+        console.log(items)
+        spanRemove.addEventListener('click', () => {
+
+        })*/
+
+    }
+})
 
 
 /*
