d6ba32730b19e2b19c8ae2bb6a99b077a24b7fba
jonathanj101 <jonatjimen@gmail.com>
2020-08-28 19:37:33 -0400

    updated!! two event listener for adding task and the other to delete tasks!!!
    updated!! two event listener for adding task and the other to delete tasks!!!

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 /* GLOBAL VARIABLES */
 
+let userTasks = []
+
+
 let userInput = document.querySelector("#user-input")
 let unordrd = document.querySelector("#main-lists__container")
-let listsItem = document.querySelector("#lists-item")
-let btnRemove = document.querySelector('#remove')
-
 
 
 /* EVENT LISTENER FOR USER INPUT*/
-userInput.addEventListener('keydown', (event) => {
+
+
+userInput.addEventListener('keydown', () => {
     if (event.keyCode === 13) {
+        //creating element at user's input 'enter'
+
         let listItem = document.createElement('li')
         let listBtnItem = document.createElement('li')
         let spanInputValue = document.createElement('span')
         let btnRemove = document.createElement('button')
 
         listItem.id = 'lists-item'
+        listItem.className = userInput.value
         btnRemove.id = 'remove'
         listBtnItem.id = 'lists-item'
         spanInputValue.id = 'inputValue'
@@ -30,32 +35,66 @@ userInput.addEventListener('keydown', (event) => {
         listBtnItem.appendChild(btnRemove)
 
         if (userInput.value === '') {
+            // if user input is empty, not to add it to the list
             alert('Empty Task, no Reminder!')
         } else {
+            // adding task to the ul list
             unordrd.insertAdjacentElement("afterbegin", listItem)
             listItem.insertAdjacentElement("beforeend", btnRemove)
             listItem.insertAdjacentElement('afterbegin', spanInputValue)
-
             btnRemove.innerHTML = '&times;'
             userInput.value = ''
-
-            /* delete task*/
-
-            btnRemove.addEventListener('click', (e) => {
-                e.target.parentElement.remove()
-            })
-            btnRemove.addEventListener('mouseover', () => {
-                if (spanInputValue.id === "inputValue") {
-                    spanInputValue.style.textDecoration = 'line-through'
-                }
-            })
-            btnRemove.addEventListener('mouseout', () => {
-                if (spanInputValue.id === 'inputValue') {
-                    spanInputValue.style.textDecoration = 'none'
-                }
-            })
-
         }
+        let listsItem = document.querySelectorAll("li")
+
+        for (let i = 0; i < listsItem.length; i++) {
+            if (listsItem.className === userInput.value) {
+                let idk = userTasks.pop(listsItem)
+                console.log(idk)
+            } else {
+                userTasks.push(listsItem)
+            }
+        }
+
+
+        console.log(userTasks)
+        console.log(listsItem)
+        console.log(userTasks)
+        /* local storage*/
+        let date = new Date()
+
+        let month = date.getMonth() + 1
+        let day = date.getDay()
+        let year = date.getFullYear()
+        let minutes = date.getMinutes()
+
+        let itemDetail = localStorage.setItem('userTasks', JSON.stringify({ task: userTasks, id: (`${month}/${day}/${year} ${minutes}`) }))
+        let getItemDetail = localStorage.getItem('userTasks')
+        const item = JSON.parse(getItemDetail)
+
     }
 
+})
+
+/* delete task*/
+userInput.addEventListener('keyup', () => {
+    if (event.keyCode === 13) {
+        let btnRemove = document.querySelector('#remove')
+        let spanInputValue = document.querySelector("#inputValue")
+        btnRemove.addEventListener('click', (e) => {
+            e.target.parentElement.remove()
+        })
+        /* mouseover event on x button to display line-through on task */
+        btnRemove.addEventListener('mouseover', () => {
+            if (spanInputValue.id === "inputValue") {
+                spanInputValue.style.textDecoration = 'line-through'
+            }
+        })
+        /* mouseout event on x button to not display line-through on task */
+        btnRemove.addEventListener('mouseout', () => {
+            if (spanInputValue.id === 'inputValue') {
+                spanInputValue.style.textDecoration = 'none'
+            }
+        })
+    }
 })
