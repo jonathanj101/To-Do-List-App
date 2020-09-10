3e7429f056e791e472937ca6b8f20faf00c40d41
jonathanj101 <jonatjimen@gmail.com>
2020-09-09 21:28:07 -0400

    refactored!!
    refactored!!

--- main.js
+++ main.js
@@ -1,107 +1,85 @@
-/* GLOBAL VARIABLES */
+document.addEventListener("DOMCOntentLoaded", function () {
+    getFromLocalStorage()
+})
 
 let userInput = document.querySelector("#user-input")
 let unordrd = document.querySelector("#main-lists__container")
 
-
-/* EVENT LISTENER FOR USER INPUT*/
-
-
 userInput.addEventListener('keydown', () => {
     if (event.keyCode === 13) {
-
-        //creating element at user's keyboard 'enter'
-
-        let listItem = document.createElement('li')
-        let listBtnItem = document.createElement('li')
-        let spanInputValue = document.createElement('span')
-        let btnRemove = document.createElement('button')
-        // adding li elements' id
-
-        listItem.id = 'lists-item'
-        listItem.className = userInput.value
-        btnRemove.id = 'remove'
-        listBtnItem.id = 'lists-item'
-        spanInputValue.id = 'inputValue'
-
-        // getting user's input
-        let spnUserText = document.createTextNode(userInput.value)
-
-
-        // appending user text to span
-        spanInputValue.appendChild(spnUserText)
-        // appending button to li element
-        listBtnItem.appendChild(btnRemove)
-
-        // if user input is empty, not to add it to the list
-        if (userInput.value === '') {
-
-            alert('Empty Task, no Reminder!')
-        } else {
-            // adding task to the ul list
-            unordrd.insertAdjacentElement("afterbegin", listItem)
-            listItem.insertAdjacentElement("beforeend", btnRemove)
-            listItem.insertAdjacentElement('afterbegin', spanInputValue)
-            btnRemove.innerHTML = '&times;'
-            userInput.value = ''
-
-        }
-
-
-        // LOCAL STORAGE
-
-        let taskId = {
-            created: `${month}/${day}/${year} ${secs}`
-        }
-
-        const listsItem = document.querySelectorAll('li')
-        const span = document.querySelectorAll('span')
-
-        let userTask = []
-        const todo = {
-            task: userTask,
-            id: `${month}/${day}/${year} ${secs}`
-        }
-        span.forEach(function (item) {
-            let date = new Date()
-
-            let year = date.getYear()
-            let month = date.getMonth() + 1
-            let day = date.getDate()
-            let secs = date.getSeconds()
-
-            userTask.push(item.textContent, taskId)
-
-        })
-
-        localStorage.setItem('Tasks', JSON.stringify(userTask))
-        task = JSON.parse(localStorage.getItem('Tasks'))
-
-        console.log(typeof (task))
-    }
-
-})
-
-/* delete task*/
-userInput.addEventListener('keyup', () => {
-    if (event.keyCode === 13) {
-        let btnRemove = document.querySelector('#remove')
-        let spanInputValue = document.querySelector("#inputValue")
-        btnRemove.addEventListener('click', (e) => {
-            e.target.parentElement.remove()
-        })
-        /* mouseover event on x button to display line-through on task */
-        btnRemove.addEventListener('mouseover', () => {
-            if (spanInputValue.id === "inputValue") {
-                spanInputValue.style.textDecoration = 'line-through'
-                spanInputValue.style.textDecorationColor = "red"
-            }
-        })
-        /* mouseout event on x button to not display line-through on task */
-        btnRemove.addEventListener('mouseout', () => {
-            if (spanInputValue.id === 'inputValue') {
-                spanInputValue.style.textDecoration = 'none'
-            }
-        })
+        addItem(userInput.value)
     }
 })
+
+let userTask = []
+
+function addItem(input) {
+    let spnUserText = document.createTextNode(input)
+    let spanInputValue = document.createElement('span')
+    spanInputValue.appendChild(spnUserText)
+    spanInputValue.id = 'inputValue'
+    let listItem = document.createElement('li')
+    listItem.id = 'lists-item'
+
+    let listBtnItem = document.createElement('li')
+    let btnRemove = document.createElement('button')
+
+    listItem.className = userInput.value
+    btnRemove.id = 'remove'
+    listBtnItem.id = 'lists-item'
+
+    listBtnItem.appendChild(btnRemove)
+
+    if (input.length < 2) {
+
+        alert('Empty Task, no Reminder!')
+    } else {
+        userTask.push(input)
+        unordrd.insertAdjacentElement("afterbegin", listItem)
+        listItem.insertAdjacentElement("beforeend", btnRemove)
+        listItem.insertAdjacentElement('afterbegin', spanInputValue)
+        btnRemove.innerHTML = '&times;'
+        userInput.value = ''
+        addToLocalStorage(userTask)
+        deleteTask(btnRemove, spanInputValue.textContent)
+
+    }
+
+}
+
+function addToLocalStorage(userTask) {
+    localStorage.setItem("Tasks", JSON.stringify(userTask))
+}
+
+function getFromLocalStorage() {
+    tasks = JSON.parse(localStorage.getItem("Tasks"))
+    if (tasks !== null) {
+        taskArray = tasks
+        tasks.forEach(task => {
+            addItem(task)
+        })
+    }
+
+}
+
+function deleteTask(btnRemove, spanInputValue) {
+    btnRemove.addEventListener('click', (e) => {
+        e.target.parentElement.remove()
+        let x = userTask.filter(selectedTask => {
+            return selectedTask !== spanInputValue
+        })
+        userTask = x
+        addToLocalStorage(userTask)
+    })
+    btnRemove.addEventListener('mouseover', () => {
+        if (spanInputValue.id === "inputValue") {
+            spanInputValue.style.textDecoration = 'line-through'
+            spanInputValue.style.textDecorationColor = "red"
+        }
+    })
+    btnRemove.addEventListener('mouseout', () => {
+        if (spanInputValue.id === 'inputValue') {
+            spanInputValue.style.textDecoration = 'none'
+        }
+    })
+}
